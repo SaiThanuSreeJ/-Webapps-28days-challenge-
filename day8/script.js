4f8ff797bc30428a91868fde45d067bbfecb18f9
SaiThanuSreeJ <saithanusreej@gmail.com>
2026-01-31 07:56:15 +0530

    change

---
+++ day8/script.js
@@ -0,0 +1,18 @@
+// let array=[10,20,30,40];
+// console.log(array);
+// let len= array.length;
+// // console.log(len);
+// // console.log(array[0]);
+// // console.log(array[2]);
+
+// array[0]=90;
+// //console.log(array[0]);
+// delete array[0];
+// console.log(array.length)
+// console.log(array)
+// array.push(88);
+// console.log(array);
+
+
+let array=["apple","ball","kite","sun"];
+
---
+++ day9/scrept.js
@@ -0,0 +1,12 @@
+const obj={
+    //like structure
+    //key:value
+
+    firstname:"J",
+    lastname:"y",
+    id:2500040250,
+    batch:y25
+
+};
+console.log(obj)
+
